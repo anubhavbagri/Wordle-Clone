56ce8050c90e2227528f44484d14f3de1a7ce129
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-03-12 00:17:00 +0530

    wip: wordle grid row

--- lib/widgets/wordle_grid.dart
+++ lib/widgets/wordle_grid.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:wordle_clone/widgets/wordle_letterbox.dart';
+
+const rows = 6;
+const wordSize = 5;
 
 class WordleGrid extends StatelessWidget {
   const WordleGrid({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final List<List<Widget>> wordleRows = List.empty(growable: true);
+
+    for (int i = 0; i < rows; i++) {
+      wordleRows.add(List.empty(growable: true));
+      for (int j = 0; j < wordSize; j++) {
+        wordleRows[i].add(WordleLetterbox());
+      }
+    }
+
     return Container(
       child: Text('grid'),
     );
