7498ce6926bc938646d3130e5d55c65bbbff1885
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-03-15 23:03:41 +0530

    chore: wrapping with provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wordle_clone/widgets/wordle_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
