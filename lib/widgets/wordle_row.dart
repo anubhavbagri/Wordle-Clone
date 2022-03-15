4857704ddebf2da38d2d863aedc96fa7ef0c15fd
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-03-15 23:03:26 +0530

    feat: change grid structure according to setting

--- lib/widgets/wordle_row.dart
+++ lib/widgets/wordle_row.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:wordle_clone/widgets/wordle_letterbox.dart';
 
-const wordSize = 5;
-
 class WordleRow extends StatelessWidget {
-  const WordleRow({Key? key}) : super(key: key);
+  final int wordSize;
+  const WordleRow({Key? key, required this.wordSize}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
