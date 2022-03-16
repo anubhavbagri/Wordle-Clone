4bc989dfc9d93d87c55963beb4c25a5e1d5b8c2d
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-03-16 20:12:52 +0530

    feat: make keys tappable

--- lib/widgets/wordle_key.dart
+++ lib/widgets/wordle_key.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wordle_clone/providers/game_state_provider.dart';
 
-class WordleKey extends StatelessWidget {
+class WordleKey extends ConsumerWidget {
   final String letter;
+
   const WordleKey(this.letter, {Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     Widget keyCap;
     double width = 60;
     if (letter == "_") {
@@ -23,19 +26,24 @@ class WordleKey extends StatelessWidget {
       );
     }
 
-    return Container(
-      width: width,
-      height: 60,
-      alignment: Alignment.center,
-      // padding: const EdgeInsets.all(10),
-      margin: const EdgeInsets.all(4),
-      decoration: const BoxDecoration(
-        color: Color.fromARGB(44, 44, 44, 44),
-        borderRadius: BorderRadius.all(
-          Radius.circular(4),
+    return GestureDetector(
+      child: Container(
+        width: width,
+        height: 60,
+        alignment: Alignment.center,
+        // padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.all(4),
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(44, 44, 44, 44),
+          borderRadius: BorderRadius.all(
+            Radius.circular(4),
+          ),
         ),
+        child: keyCap,
       ),
-      child: keyCap,
+      onTap: () {
+        ref.read(gameStateProvider.notifier).updateCurrentAttempt(letter);
+      },
     );
   }
 }
