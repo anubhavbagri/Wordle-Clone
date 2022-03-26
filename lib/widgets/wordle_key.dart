69e19bd85d61e9ab6672386356b9611041b22483
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-03-26 18:56:29 +0530

    fix: responsive keyboard

--- lib/widgets/wordle_key.dart
+++ lib/widgets/wordle_key.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 import 'package:wordle_clone/providers/game_state_provider.dart';
 
 class WordleKey extends ConsumerWidget {
@@ -9,40 +10,50 @@ class WordleKey extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    Widget keyCap;
-    double width = 60;
-    if (letter == "_") {
-      keyCap = const Icon(Icons.keyboard_return, size: 20);
-    } else if (letter == "<") {
-      keyCap = const Icon(Icons.backspace_outlined, size: 20);
-    } else {
-      width = 40;
-      keyCap = Text(
-        letter.toUpperCase(),
-        style: const TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
-      );
-    }
-
-    return InkWell(
-      child: Container(
-        width: width,
-        height: 55,
-        alignment: Alignment.center,
-        margin: const EdgeInsets.all(3),
-        decoration: const BoxDecoration(
-          color: Color.fromARGB(44, 44, 44, 44),
-          borderRadius: BorderRadius.all(
-            Radius.circular(4),
+    return ResponsiveBuilder(builder: (context, sizingInformation) {
+      Widget keyCap;
+      double width =
+          sizingInformation.deviceScreenType == DeviceScreenType.mobile
+              ? 50
+              : 60;
+      if (letter == "_") {
+        keyCap = const Icon(Icons.keyboard_return, size: 20);
+      } else if (letter == "<") {
+        keyCap = const Icon(Icons.backspace_outlined, size: 20);
+      } else {
+        width = sizingInformation.deviceScreenType == DeviceScreenType.mobile
+            ? 30
+            : 40;
+        keyCap = Text(
+          letter.toUpperCase(),
+          style: TextStyle(
+            fontSize:
+                sizingInformation.deviceScreenType == DeviceScreenType.mobile
+                    ? 18
+                    : 20,
+            fontWeight: FontWeight.bold,
           ),
+        );
+      }
+
+      return InkWell(
+        child: Container(
+          width: width,
+          height: 55,
+          alignment: Alignment.center,
+          margin: const EdgeInsets.all(3),
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(44, 44, 44, 44),
+            borderRadius: BorderRadius.all(
+              Radius.circular(4),
+            ),
+          ),
+          child: keyCap,
         ),
-        child: keyCap,
-      ),
-      onTap: () {
-        ref.read(gameStateProvider.notifier).updateCurrentAttempt(letter);
-      },
-    );
+        onTap: () {
+          ref.read(gameStateProvider.notifier).updateCurrentAttempt(letter);
+        },
+      );
+    });
   }
 }
